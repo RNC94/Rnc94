a2095ad516130aad30980dc3a9b84dec1875d41b
RNC94 <rodrigo_nunes@live.com>
2025-01-28 19:46:07 -0500

    Add files via upload

---
+++ packages/wrappers/src/torrentio.ts
@@ -0,0 +1,129 @@
+import { AddonDetail, StreamRequest } from '@aiostreams/types';
+import { ParsedStream, Config } from '@aiostreams/types';
+import { BaseWrapper } from './base';
+import { addonDetails } from '@aiostreams/utils';
+import { Settings } from '@aiostreams/utils';
+
+export class Torrentio extends BaseWrapper {
+  constructor(
+    configString: string | null,
+    overrideUrl: string | null,
+    addonName: string = 'Torrentio',
+    addonId: string,
+    userConfig: Config,
+    indexerTimeout?: number
+  ) {
+    let url = overrideUrl
+      ? overrideUrl
+      : Settings.TORRENTIO_URL + (configString ? configString + '/' : '');
+
+    super(
+      addonName,
+      url,
+      addonId,
+      userConfig,
+      indexerTimeout || Settings.DEFAULT_TORRENTIO_TIMEOUT
+    );
+  }
+}
+
+export async function getTorrentioStreams(
+  config: Config,
+  torrentioOptions: {
+    useMultipleInstances?: string;
+    overrideUrl?: string;
+    indexerTimeout?: string;
+    overrideName?: string;
+  },
+  streamRequest: StreamRequest,
+  addonId: string
+): Promise<ParsedStream[]> {
+  const supportedServices: string[] =
+    addonDetails.find((addon: AddonDetail) => addon.id === 'torrentio')
+      ?.supportedServices || [];
+  const parsedStreams: ParsedStream[] = [];
+  const indexerTimeout = torrentioOptions.indexerTimeout
+    ? parseInt(torrentioOptions.indexerTimeout)
+    : undefined;
+
+  // If overrideUrl is provided, use it to get streams and skip all other steps
+  if (torrentioOptions.overrideUrl) {
+    const torrentio = new Torrentio(
+      null,
+      torrentioOptions.overrideUrl as string,
+      torrentioOptions.overrideName,
+      addonId,
+      config,
+      indexerTimeout
+    );
+    return torrentio.getParsedStreams(streamRequest);
+  }
+
+  // find all usable services
+  const usableServices = config.services.filter(
+    (service) => supportedServices.includes(service.id) && service.enabled
+  );
+
+  // if no usable services found, use torrentio without any configuration
+  if (usableServices.length < 1) {
+    const torrentio = new Torrentio(
+      null,
+      null,
+      torrentioOptions.overrideName,
+      addonId,
+      config,
+      indexerTimeout
+    );
+    return await torrentio.getParsedStreams(streamRequest);
+  }
+
+  // otherwise, depending on the configuration, create multiple instances of torrentio or use a single instance with all services
+
+  const getServicePair = (
+    serviceId: string,
+    credentials: { [key: string]: string }
+  ) => {
+    return serviceId === 'putio'
+      ? `${serviceId}=${credentials.clientId}@${credentials.token}`
+      : `${serviceId}=${credentials.apiKey}`;
+  };
+
+  if (torrentioOptions.useMultipleInstances === 'true') {
+    const promises = usableServices.map(async (service) => {
+      if (!service.enabled) {
+        return [];
+      }
+      console.log('Creating Torrentio instance with service:', service.id);
+      let configString = getServicePair(service.id, service.credentials);
+      const torrentio = new Torrentio(
+        configString,
+        null,
+        torrentioOptions.overrideName,
+        addonId,
+        config,
+        indexerTimeout
+      );
+      return await torrentio.getParsedStreams(streamRequest);
+    });
+    const results = await Promise.all(promises);
+    results.forEach((streams) => parsedStreams.push(...streams));
+    return parsedStreams;
+  } else {
+    let configString = '';
+    for (const service of usableServices) {
+      if (!service.enabled) {
+        continue;
+      }
+      configString += getServicePair(service.id, service.credentials) + '|';
+    }
+    const torrentio = new Torrentio(
+      configString,
+      null,
+      torrentioOptions.overrideName,
+      addonId,
+      config,
+      indexerTimeout
+    );
+    return await torrentio.getParsedStreams(streamRequest);
+  }
+}
